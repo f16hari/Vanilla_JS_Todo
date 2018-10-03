3d4359108bd4c2c8455e25807380b785ea6391f7
Hariharan J <aeromasterhari15@gmail.com>
2018-10-03 21:59:08 +0530

    Fourth

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,26 @@
-var trash = '<i class="fa fa-trash" aria-hidden="true"></i>';
-var tick = '<i class="fa fa-check-circle" aria-hidden="true"></i>';
-var redo = '<i class="fa fa-repeat" aria-hidden="true"></i>';
+const trash = '<i class="fa fa-trash" aria-hidden="true"></i>';
+const tick = '<i class="fa fa-check-circle" aria-hidden="true"></i>';
+const redo = '<i class="fa fa-repeat" aria-hidden="true"></i>';
 
-var count=document.getElementById('notDone').childElementCount;
-var count1 = document.getElementById('Done').childElementCount;
-var empty = document.getElementById('empty');
-var cempty = document.getElementById('cempty');
-var ndheading = document.getElementById('ndheading');
-var dheading = document.getElementById('dheading');
+const ndheading = document.getElementById('ndheading');
+const dheading = document.getElementById('dheading');
 
-if(count==0){
-    empty.innerHTML='Good !! Enter New Activity';
-    ndheading.innerHTML='';
-}
-
-if(count1==0){
-    cempty.innerHTML='No Completed Activity !!';
-}
-
-function check()
+const check = () =>
 {
-    var count=document.getElementById('notDone').childElementCount;
-    var count1 = document.getElementById('Done').childElementCount;
-    console.log(count);
-    console.log(count1);
-    if(count==0){
-        
-        empty.innerHTML='Good !! Enter New Activity';
-        ndheading.innerHTML='';
+    let count=document.getElementById('notDone').childElementCount;
+    let count1 = document.getElementById('Done').childElementCount;
     
-    }
-    else
-    {
-        empty.innerHTML='';
-        ndheading.innerHTML='Activity Pending';
-    }
+    if(count==0){ndheading.innerHTML='';}
+    else{ndheading.innerHTML=''; ndheading.appendChild(document.createTextNode('Activity Pending'));}
     
-
-    if(count1==0){
-        
-        cempty.innerHTML='No Completed Activity !!';
-        dheading.innerHTML='';
-    
-    }
-    else
-    {
-        cempty.innerHTML='';
-        dheading.innerHTML='Completed Activity';
-    }
-        
-
-
+    if(count1==0){dheading.innerHTML='';}
+    else{dheading.innerHTML='';dheading.appendChild(document.createTextNode('Completed Activity'));}
 }
 
 
 document.getElementById('add').onclick=function print(){
 
-    var value = document.getElementById('newActivity').value;
-    count=document.getElementById('notDone').childElementCount;
-    if(count!=0){empty.innerHTML='';}
+    const value = document.getElementById('newActivity').value;
     
     if(value){ 
         addActivity(value);
@@ -67,7 +29,7 @@ document.getElementById('add').onclick=function print(){
     }
 }
 
-function removeItem()
+const removeItem = () =>
 {
     var Item = this.parentNode.parentNode;
     var parent = Item.parentNode;
@@ -75,6 +37,40 @@ function removeItem()
     check();
 }
 
+function addActivity(text)
+{
+    var list = document.getElementById('notDone');
+
+    var item = document.createElement('li');
+
+    var data = document.createElement('p');
+    data.classList.add('Activity');
+    data.innerHTML = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var button1 = document.createElement('button');
+    button1.classList.add('delete');
+    button1.innerHTML = trash;
+
+    button1.addEventListener('click',removeItem);
+
+    var button2 = document.createElement('button');
+    button2.classList.add('completed');
+    button2.innerHTML = tick;
+
+    button2.addEventListener('click',adCompleted);
+
+    list.insertBefore(item, list.childNodes[0]);
+    item.appendChild(data);
+    item.appendChild(buttons);
+    buttons.appendChild(button1);
+    buttons.appendChild(button2);
+    check();
+
+
+}
 function adCompleted()
 {
     var par= this.parentNode.parentNode;
@@ -115,40 +111,7 @@ function adCompleted()
 
 }
 
-function addActivity(text)
-{
-    var list = document.getElementById('notDone');
 
-    var item = document.createElement('li');
-
-    var data = document.createElement('p');
-    data.classList.add('Activity');
-    data.innerHTML = text;
-
-    var buttons = document.createElement('div');
-    buttons.classList.add('buttons');
-
-    var button1 = document.createElement('button');
-    button1.classList.add('delete');
-    button1.innerHTML = trash;
-
-    button1.addEventListener('click',removeItem);
-
-    var button2 = document.createElement('button');
-    button2.classList.add('completed');
-    button2.innerHTML = tick;
-
-    button2.addEventListener('click',adCompleted);
-
-    list.insertBefore(item, list.childNodes[0]);
-    item.appendChild(data);
-    item.appendChild(buttons);
-    buttons.appendChild(button1);
-    buttons.appendChild(button2);
-    check();
-
-
-}
 
 function reDo()
 {
