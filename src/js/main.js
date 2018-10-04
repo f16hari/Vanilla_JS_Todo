50a0ab60b8fbf764df336d317e9df5bd58aac2f0
Hariharan J <aeromasterhari15@gmail.com>
2018-10-04 22:14:02 +0530

    fifth

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ const trash = '<i class="fa fa-trash" aria-hidden="true"></i>';
 const tick = '<i class="fa fa-check-circle" aria-hidden="true"></i>';
 const redo = '<i class="fa fa-repeat" aria-hidden="true"></i>';
 
+const list1 = document.getElementById('notDone');
+const list2 = document.getElementById('Done');
 const ndheading = document.getElementById('ndheading');
 const dheading = document.getElementById('dheading');
 
@@ -23,133 +25,48 @@ document.getElementById('add').onclick=function print(){
     const value = document.getElementById('newActivity').value;
     
     if(value){ 
-        addActivity(value);
+        updateList(list1 , value , tick);
         document.getElementById('newActivity').value='';
-
     }
 }
 
-const removeItem = () =>
+function updateList(listid , text , icon)
 {
-    var Item = this.parentNode.parentNode;
-    var parent = Item.parentNode;
-    parent.removeChild(Item);
-    check();
-}
+    const item = document.createElement('li');
 
-function addActivity(text)
-{
-    var list = document.getElementById('notDone');
-
-    var item = document.createElement('li');
-
-    var data = document.createElement('p');
+    const data = document.createElement('p');
     data.classList.add('Activity');
     data.innerHTML = text;
 
-    var buttons = document.createElement('div');
+    const buttons = document.createElement('div');
     buttons.classList.add('buttons');
 
-    var button1 = document.createElement('button');
+    const button1 = document.createElement('button');
     button1.classList.add('delete');
     button1.innerHTML = trash;
 
     button1.addEventListener('click',removeItem);
 
-    var button2 = document.createElement('button');
+    const button2 = document.createElement('button');
     button2.classList.add('completed');
-    button2.innerHTML = tick;
+    button2.innerHTML = icon;
+    if(listid.id == 'notDone')
+        button2.addEventListener('click',function(){this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);updateList(list2 , text , redo);});
+    if(listid.id == 'Done')
+        button2.addEventListener('click',function(){this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);updateList(list1 , text , tick)});
 
-    button2.addEventListener('click',adCompleted);
-
-    list.insertBefore(item, list.childNodes[0]);
+    listid.insertBefore(item, listid.childNodes[0]);
     item.appendChild(data);
     item.appendChild(buttons);
     buttons.appendChild(button1);
     buttons.appendChild(button2);
     check();
-
-
-}
-function adCompleted()
-{
-    var par= this.parentNode.parentNode;
-    var compData = par.querySelector('p').innerHTML;
-
-    par.parentNode.removeChild(par);
-    check();
-
-    var list = document.getElementById('Done');
-    var item = document.createElement('li');
-
-    var data = document.createElement('p');
-    data.classList.add('Activity');
-    data.innerHTML = compData;
-
-    var buttons = document.createElement('div');
-    buttons.classList.add('buttons');
-
-    var button1 = document.createElement('button');
-    button1.classList.add('delete');
-    button1.innerHTML = trash;
-
-    button1.addEventListener('click',removeItem);
-
-    var button2 = document.createElement('button');
-    button2.classList.add('redo');
-    button2.innerHTML = redo;
-
-    button2.addEventListener('click',reDo);
-
-    list.insertBefore(item, list.childNodes[0]);
-    item.appendChild(data);
-    item.appendChild(buttons);
-    buttons.appendChild(button1);
-    buttons.appendChild(button2);
-    check();
-
-
 }
 
-
-
-function reDo()
+function removeItem()
 {
-
-    var par= this.parentNode.parentNode;
-    var compData = par.querySelector('p').innerHTML;
-
-    par.parentNode.removeChild(par);
-
-    var list = document.getElementById('notDone');
-    var item = document.createElement('li');
-
-    var data = document.createElement('p');
-    data.classList.add('Activity');
-    data.innerHTML = compData;
-
-    var buttons = document.createElement('div');
-    buttons.classList.add('buttons');
-
-    var button1 = document.createElement('button');
-    button1.classList.add('delete');
-    button1.innerHTML = trash;
-
-    button1.addEventListener('click',removeItem);
-
-    var button2 = document.createElement('button');
-    button2.classList.add('completed');
-    button2.innerHTML = tick;
-
-    button2.addEventListener('click',adCompleted);
-
-    list.insertBefore(item, list.childNodes[0]);
-    item.appendChild(data);
-    item.appendChild(buttons);
-    buttons.appendChild(button1);
-    buttons.appendChild(button2); 
-
+    const Item = this.parentNode.parentNode;
+    const parent = Item.parentNode;
+    parent.removeChild(Item);
     check();
-
-
 }
